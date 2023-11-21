e4a7def76ec6de5179b16e96db00cd908f42ef20
Bart Duisters <bartduisters@bartduisters.com>
2023-11-21 15:35:39 +0100

    oplossing 2: zelfde document (herbruikbare setCounter)

--- src/counter.ts
+++ src/counter.ts
@@ -1,12 +1,13 @@
 let counter = 0;
 
+const setCounter = (count: number, element: Element) => {
+  counter = count;
+  element.innerHTML = `count is ${counter}`;
+};
+
 export function setupCounter(element: HTMLButtonElement) {
-  const setCounter = (count: number) => {
-    counter = count;
-    element.innerHTML = `count is ${counter}`;
-  };
-  element.addEventListener("click", () => setCounter(counter + 1));
-  setCounter(0);
+  element.addEventListener("click", () => setCounter(counter + 1, element));
+  setCounter(0, element);
 }
 
 export function setupDoubleCounter(
@@ -15,7 +16,7 @@ export function setupDoubleCounter(
 ) {
   element.innerHTML = "+2";
   element.addEventListener("click", () => {
-    counter = counter + 2;
-    countButton.innerHTML = `count is ${counter}`;
+    const newCount = counter + 2;
+    setCounter(newCount, countButton);
   });
 }
